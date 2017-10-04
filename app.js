51e0d8c6af989ef0e461fb0872d4ab4b33d5460c
Mirwais <mirwais_fazli@live.nl>
2017-10-04 03:19:10 +0200

    Initialize
    - Created book and genre models.
    - Created app.js file with routes.
    - Filled database with dummy data.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+Genre = require('./models/genre');
+Book = require('./models/book');
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/bookstore', {useMongoClient: true});
+var db = mongoose.connection;
+
+// Route for landing page
+app.get('/', function(req, res){
+    res.send('Please use /api/books or /api/genres to navigate through the API.');
+});
+
+app.get('/api/genres', function(req, res) {
+    Genre.getGenres(function(err, genres) {
+        if(err) {
+            throw err;
+        }
+        res.json(genres);
+    });
+})
+
+app.get('/api/books', function(req, res) {
+    Book.getBooks(function(err, books) {
+        if(err) {
+            throw err;
+        }
+        res.json(books);
+    });
+})
+
+app.listen(3000);
+console.log('Running on port 3000...');
